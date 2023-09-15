f16784275b0cf23c2a228df52503e10f50d556cb
lucky15100 <131132214+lucky15100@users.noreply.github.com>
2023-09-15 11:47:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
+// Function to highlight bold words in a specific color
 function highlight() {
-    //Write your code here
+    // Get all <strong> elements in the second paragraph
+    const boldElements = document.querySelectorAll('p:nth-child(2) strong');
 
-
+    // Loop through each <strong> element and change its color
+    for (const boldElement of boldElements) {
+        boldElement.style.color = 'green'; // Change the color to your desired highlight color
+    }
 }
 
-
+// Function to return bold words to their normal color
 function return_normal() {
-    //Write your code here
+    // Get all <strong> elements in the second paragraph
+    const boldElements = document.querySelectorAll('p:nth-child(2) strong');
 
-    
+    // Loop through each <strong> element and change its color back to its original color
+    for (const boldElement of boldElements) {
+        boldElement.style.color = 'black'; // Change the color back to the normal color
+    }
 }
+
+// Attach the hover event handlers to the link
+const link = document.querySelector('a');
+
+// Add the 'mouseenter' event handler to highlight on hover
+link.addEventListener('mouseenter', highlight);
+
+// Add the 'mouseleave' event handler to return to normal color on hover out
+link.addEventListener('mouseleave', return_normal);
